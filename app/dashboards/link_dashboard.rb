bced83ccf39ae6d53353fe82f46161fc453ad813
Royal Summers <rcrc001@gmail.com>
2018-01-17 09:50:25 -0700

    Initial Commit

---
+++ app/dashboards/link_dashboard.rb
@@ -0,0 +1,63 @@
+require "administrate/base_dashboard"
+
+class LinkDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    votes_for: Field::HasMany.with_options(class_name: "ActsAsVotable::Vote"),
+    user: Field::BelongsTo,
+    comments: Field::HasMany,
+    id: Field::Number,
+    title: Field::String,
+    url: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :votes_for,
+    :user,
+    :comments,
+    :id,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :votes_for,
+    :user,
+    :comments,
+    :id,
+    :title,
+    :url,
+    :created_at,
+    :updated_at,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :votes_for,
+    :user,
+    :comments,
+    :title,
+    :url,
+  ].freeze
+
+  # Overwrite this method to customize how links are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(link)
+  #   "Link ##{link.id}"
+  # end
+end
